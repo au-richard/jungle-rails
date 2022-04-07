a071c794eade89efdff926a0f6c3e9558b0d8466
Richard <au.richard4@gmail.com>
2022-04-07 16:27:56 -0700

    Adding adming dashboard to display total product count and total number of categories.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    # declare instance variables inside here
+    @num_products = Product.count
+    @num_categories = Category.count
   end
 end
